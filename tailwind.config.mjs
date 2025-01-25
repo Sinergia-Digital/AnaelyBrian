b51137ba87db59993223eeabab6460aa9ce3bf39
vabklb <victor.barilin@gmail.com>
2025-01-25 07:29:54 -0300

    new invitation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'background-color': '#6c8778',
+				'text-color': '#171717',
+				'primary-color': '#9c7a32',
+				'secondary-color': '#716e6c',
+				'botton-color': 'white',
+				'botton-color-hover': 'black',
+				'botton-text-color': '#333'
+			}
+		},
 	},
 	plugins: [],
 }
